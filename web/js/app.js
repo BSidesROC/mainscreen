7e0eb1d0e5414961d23718ad53e8dccabbf197bc
Jason Ross <algorythm@gmail.com>
2015-04-11 18:59:22 -0400

    initial commit

---
+++ web/js/app.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function() {
+
+    // tweak 'hits'
+    $('.hit').html('&nbsp;&nbsp;');
+    $('.hit').css('border', 'none');
+    $('.hit').css('background-color', 'inherit');
+
+    // tweak 'misses'
+    $('.miss').html('&nbsp;&nbsp;');
+    $('.miss').css('border', 'none');
+    $('.miss').css('background-color', 'inherit');
+
+});
